e3dbf2646acc9674f815fbfc2dd2b819c9869c24
CescoMoretti <71732702+CescoMoretti@users.noreply.github.com>
2022-01-13 13:12:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -74,6 +74,8 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'Flask==1.1.2',
+        'Flask-SQLAlchemy==2.4.4',
     ],
     extras_require={
         # eg:
