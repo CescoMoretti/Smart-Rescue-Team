a0c65ed42c32b80ffbf05b92bbfbb1518b95f55c
CescoMoretti <71732702+CescoMoretti@users.noreply.github.com>
2022-01-19 20:54:25 +0100

    test db

--- src/server/app.py
+++ src/server/app.py
@@ -1,5 +1,12 @@
+from pathlib import Path
+import sys
+path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
+sys.path.insert(0, path)
+
+from pickle import GET
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 
 
 #create a flask instance
@@ -8,6 +15,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 #Initialize database
 db = SQLAlchemy(app)
+#Create data model
+class Db_data_model(db.Model):
+    id =        db.Column(db.Integer, nullable=False, primary_key=True)
+    name =      db.Column(db.String,  nullable=False)
+    type =      db.Column(db.String,  nullable=False)
+    gps_lat =   db.Column(db.Integer, nullable=False)
+    gps_long =  db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.Integer)
+    battery =   db.Column(db.Integer)
 
 
 #Create a route decorator
@@ -20,3 +36,13 @@ def index():
 
 def user(name):
     return render_template("user.html", user_name=name)
+
+@app.route('/data/add', methods=['GET', 'POST'])
+
+def add_data():
+    data = Db_data_model(name= "1", type = "team", gps_lat = 12, gps_long = 145,)
+    db.session.add(data)
+    db.session.commit()
+    istances = Db_data_model.query.order_by(Db_data_model.id)
+    print(istances)
+    return render_template("add_data.html", istances=istances)
--- src/utils/models/db_data_model.py
+++
@@ -1,9 +0,0 @@
-from server.app import db
-
-class Db_data_model(db.Model):
-    id =        db.Column(db.Integer, nullable=False, unique=True)
-    type =      db.Column(db.String, nullable=False)
-    gps_lat =   db.Column(db.Integer, nullable=False)
-    gps_long =  db.Column(db.Integer, nullable=False)
-    timestamp = db.Column(db.Datetime, nullable=False)
-    battery =   db.Column(db.Integer)
