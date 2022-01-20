a29b9fe20a87686d01123e5b97d75f13e66eb89f
CescoMoretti <71732702+CescoMoretti@users.noreply.github.com>
2022-01-20 01:03:08 +0100

    iniziato caricamento immagine

--- src/server/app.py
+++ src/server/app.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from pathlib import Path
 import sys
 path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
@@ -5,12 +6,28 @@ sys.path.insert(0, path)
 
 from pickle import GET
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_wtf.file import FileField, FileRequired
+from wtforms.validators import InputRequired, DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
 
 
 #create a flask instance
 app = Flask(__name__)
+#Key for forms
+
+app.config['SECRET_KEY'] = "password" #In teoria andrebbe nascosta
+#Create form class
+class Image_form(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    #image = FileField('submit image', validators=[FileRequired()]) # IMAGE
+    submit = SubmitField("Add Map")
+
+
+
+
 #App database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 #Initialize database
@@ -45,8 +62,24 @@ def add_data():
     istances = Db_data_model.query.order_by(Db_data_model.name)
     
     return render_template("add_data.html", istances=istances)
+ 
+@app.route('/images', methods=['GET', 'POST'])
 
+def add_image():  
+    name = None
+    #image = None
+    form = Image_form()
+    if form.validate_on_submit():
+        print(form.name.data)
+        name = form.name.data
+        #image = form.image.data
+        form.name.data = ''
+        
 
+    return render_template("images.html",     
+                            name = name,
+                            #image = image,
+                            form = form)
 
 
 #Error pages
