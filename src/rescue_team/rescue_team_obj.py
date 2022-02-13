d0cb2a32b0b76ea8a89823c6b935a6127413fffd
tobi1modna <tobia.poppi@virgilio.it>
2022-02-13 11:20:44 +0100

    showed ai received ack to rescue teams fixed some details

--- src/rescue_team/rescue_team_obj.py
+++ src/rescue_team/rescue_team_obj.py
@@ -1,13 +1,19 @@
+import binascii
+from doctest import DONT_ACCEPT_BLANKLINE
 from pathlib import Path
 import sys
 path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
 sys.path.insert(0, path)
 from utils.msgs.msg_team_telemetry import Msg_team_telemetry
+from utils.msgs.msg_dog_matchingAI import Msg_dog_matchingAI
 from utils.models.subscriber_mqtt import Subscriber_mqtt
 from utils.models.thread_model import Thread_model
 from utils.data_structures.data_coordinates import Data_coordinates
 import time
 import requests
+import json, os
+
+this_path = os.getcwd()
 
 class Rescue_team_obj:
     def __init__(self, broker, port,  client_id):
@@ -18,6 +24,8 @@ class Rescue_team_obj:
         self.topic_ai = 'smart_rescue_team/+/ai_result'       
         self.subscriber_tel = Subscriber_mqtt(self.broker, self.port, self.topic_tel, self.client_id + "-tel", self.callback_dog_tel)
         self.subscriber_ai = Subscriber_mqtt(self.broker, self.port, self.topic_ai, self.client_id + "-ai", self.callback_dog_ai)
+        self.progressive_imgId = 0
+        
 
 
     def start(self):
@@ -39,18 +47,35 @@ class Rescue_team_obj:
     
     def send_data_telemetry(self): 
         msg = Msg_team_telemetry(self.client_id, self.read_coordinates())             
-        r = requests.post("http://127.0.0.1:5000/data/add/"+ msg.get_json_from_dict())
-        print("data team position sended to server: " + str(r.status_code), r.reason)
+        #r = requests.post("http://127.0.0.1:5000/data/add/"+ msg.get_json_from_dict())
+        #print("data team position sended to server: " + str(r.status_code), r.reason)
         time.sleep(2)
 
 
     def callback_dog_tel(self, data):        
-        r = requests.post("http://127.0.0.1:5000/data/add/"+ data)
-        print("data receved sended to server: " + r.status_code, r.reason)
+        #r = requests.post("http://127.0.0.1:5000/data/add/"+ data)
+        #print("data receved sended to server: " + r.status_code, r.reason)
+        pass
+
 
     def callback_dog_ai(self, data):
-        r = requests.post("http://127.0.0.1:5000/data/add/"+ data)
-        print("data receved sended to server: " + str(r.status_code), r.reason)
+        #r = requests.post("http://127.0.0.1:5000/data/add/"+ data)
+        #print("data receved sended to server: " + str(r.status_code), r.reason)
+        data_json = json.loads(data)
+        #msg = Msg_dog_matchingAI(data_json["name"],
+                                #data_json["gps"],
+                                #data_json["img"],
+                                #data_json["imgname"],
+                                #data_json["ack"])
+        
+        if data_json['ack'] == True:
+            self.progressive_imgId += 1
+            with open(this_path+'/src/rescue_team/received_positive_imgs/'+str(self.progressive_imgId)+'.jpg', 'wb') as fw:
+                fw.write(binascii.a2b_base64(data_json['img']))
+
+            print('Detected People by dog '+str(data_json['name'])+'!\nAt time '+str(data_json['timestamp']))
+            
+
 
     def read_coordinates(self):
         #TODO
